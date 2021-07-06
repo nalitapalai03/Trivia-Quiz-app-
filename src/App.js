b54abcf6b262a57186e4c696060249cc3202dff3
Nalita Palai <58192788+nalitapalai03@users.noreply.github.com>
2021-07-06 18:38:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React,{useState}from 'react';
+import QuestionList from './components/QuestionList';
+import {v4 as uuidv4} from "uuid";
+import "./App.css";
+
+const App = () => {
+
+  const [currentQuestion,setCurrentQuestion]=useState(0);
+  const [score,setScore]=useState(0);
+  const [click,setClicked]=useState(false);
+  const [showScore,setShowScore]=useState(false);
+
+  const handleCorrectAnswer=(isCorrect)=>{
+    if(isCorrect){
+      setScore(score+1);
+    }
+    setClicked(true);
+  };
+
+  const handleNextQuestion=()=>{
+    setClicked(false);
+    if(currentQuestion<QuestionList.length - 1){
+      setCurrentQuestion(currentQuestion+1)
+    }else{
+      setShowScore(true);
+    }
+  }
+
+  return (
+  <div className="app-wrapper">
+    {showScore?(
+      <div>
+        <div className="completed">Completed</div>
+        <div className="score-section">
+          Your Score: {score}/{QuestionList.length}
+        </div>
+
+      </div>
+    ):(
+    <div>
+    <div className="question-section-wrapper">
+      <div className="question-count">
+        Question {currentQuestion + 1} of {QuestionList.length}
+      </div>
+      <div className="question">
+        {QuestionList[currentQuestion].question}
+      </div>
+
+    </div>
+    <div className="answer-section-wrapper">
+      {QuestionList[currentQuestion].answerList.map((answerOption)=>(
+        <li className="answer-list" key={uuidv4()}>
+      <button className={`answer-button ${click && answerOption.isCorrect?"correct":""}`}
+      onClick={()=>handleCorrectAnswer(answerOption.isCorrect)}
+      >
+        {answerOption.answer}
+        </button>
+        </li>
+      ))}
+    </div>
+  
+  <div>
+    <button className="next-button" onClick={handleNextQuestion}>Next</button>
+  </div>
+  </div>
+  )}
+  </div>
+  );
+};
+
+export default App
---
+++ src/components/QuestionList.js
@@ -0,0 +1,43 @@
+const QuestionList = [{
+        question: "WHat is the height of Mt. Everest?",
+        answerList: [
+            { answer: "8848", isCorrect: true },
+            { answer: "10147", isCorrect: false },
+            { answer: "3687", isCorrect: false },
+            { answer: "7895", isCorrect: false },
+
+        ],
+    },
+    {
+        question: "What's Ussain bolt's to speed?",
+        answerList: [
+            { answer: "22.7m/s", isCorrect: true },
+            { answer: "35m/s", isCorrect: false },
+            { answer: "45m/s", isCorrect: false },
+            { answer: "10m/s", isCorrect: false },
+
+        ],
+    },
+    {
+        question: "What is italia name for tomato??",
+        answerList: [
+            { answer: "tamatina", isCorrect: false },
+            { answer: "agilio", isCorrect: false },
+            { answer: "pomodorp", isCorrect: true },
+            { answer: "cipolla", isCorrect: false },
+
+        ],
+    },
+    {
+        question: "Which American president appears on one dollar???",
+        answerList: [
+            { answer: "George Washington ", isCorrect: true },
+            { answer: "Benjamin Franklin", isCorrect: false },
+            { answer: "Abraham Lincon", isCorrect: false },
+            { answer: "Barak Obama", isCorrect: false },
+
+        ],
+    },
+];
+
+export default QuestionList;
